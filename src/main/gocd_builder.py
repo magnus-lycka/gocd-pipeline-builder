d7e244cf1ebf91f68bc11cc78b37fb2022d9d6a6
Emily Bache <emily.bache@pagero.com>
2015-09-23 11:43:38 +0200

    Added some command line arguments to the script so we can get the settings and template files

--- src/main/gocd_builder.py
+++ src/main/gocd_builder.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
 
-print("Hello world!")
+def main(template_file, settings_file):
+	print("Hello World!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="turns a template file into a gocd pipeline")
+    parser.add_argument("--template-file", '-t', help="Use this template file as the basis for a new go cd pipeline")
+    parser.add_argument("--settings-file", '-s', help="use this settings file to insert values into the template file given")
+
+    parsed_args = parser.parse_args()
+    args_as_dict = vars(parsed_args) # convert Namespace object to python dictionary
+
+    print "will create pipeline using template {template_file} and settings {settings_file}".format(**args_as_dict)
+    main(**args_as_dict)
